057e55ced7140ec6968b0603aca6a61a6c159ab9
Edwin Burbano <edwinburbano473@gmail.com>
2024-05-03 10:51:46 -0500

    Token consulta guardado en una cookie

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,22 @@
+"use client"
 import getQueryToken from "@/utils/query_token";
+import { useEffect, useState } from "react";
 
-export default async function HomePage() {
-  const token = await getQueryToken()
+export default function HomePage() {
+  const [token, setToken] = useState("")
+
+  useEffect(()=> {
+    const getToken = async () => {
+      const token = await getQueryToken()
+      setToken(token)
+    }
+    getToken()
+  }, [])
   console.log(token)
   return (
     <div>
       HOME PAGE
+      <p>su token es: {token}</p>
     </div>
   );
 }
--- src/utils/query_token.js
+++ src/utils/query_token.js
@@ -1,22 +1,19 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 
-const NIT = "817005385";
-const TOKEN = "F6BF055E-4200-43E4-89AA-198B0A66010D";
-const API_URL = "https://wsmipres.sispro.gov.co/WSSUMMIPRESNOPBS/api";
-
-async function getQueryToken() {
+ export default async function getQueryToken() {
     try {
-        const res = await axios(`${API_URL}/GenerarToken/${NIT}/${TOKEN}`)
-        if (res.status !== 200) {
-            throw new Error(`Fallo en la respuesta HTTP con estado ${res.status}`)
-        } 
-        const queryToken = res.data;
-        return queryToken
+        const queryToken = Cookies.get("queryToken") // Buscar el token en la cookie
+        if (queryToken) { return queryToken } // Si hay un token en la cookie retorna ese toke
+        const res = await axios(`${process.env.NEXT_PUBLIC_API_URL}/GenerarToken/${process.env.NEXT_PUBLIC_NIT}/${process.env.NEXT_PUBLIC_TOKEN}`) // Petición a la api
+        if (res.status !== 200) { throw new Error(`Fallo en la respuesta HTTP con estado ${res.status}`) } // Manejo de errores
+        const newQueryToken = res.data;
+        Cookies.set('queryToken', newQueryToken) // Agregar el nuevo token a la cookie
+        return newQueryToken 
     } catch (error) {
         console.log( "Error al intentar obtener el token de consulta", error)
         return null
     }
 }
 
-export default getQueryToken
 
